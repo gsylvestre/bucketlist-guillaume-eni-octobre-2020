3df92c61f0a21bde014df81c1861bdaf9b66fbca
Guillaume <gsylvestre@gmail.com>
2020-10-01 09:35:45 +0200

    correction tp 5

--- src/Controller/IdeaController.php
+++ src/Controller/IdeaController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Idea;
+use App\Repository\IdeaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IdeaController extends AbstractController
@@ -10,25 +13,30 @@ class IdeaController extends AbstractController
     /**
      * @Route("/ideas", name="idea_list")
      */
-    public function list()
+    public function list(IdeaRepository $ideaRepository)
     {
-        //@todo: aller chercher toutes les dernières idées dans la bdd
+        //récupère les 50 idées publiées les plus récentes
+        $ideas = $ideaRepository->findBy(["isPublished" => true], ["dateCreated" => "DESC"], 50);
 
         return $this->render('idea/list.html.twig', [
-
+            "ideas" => $ideas
         ]);
     }
 
     /**
      * @Route("/ideas/details/{id}", name="idea_details", requirements={"id": "\d+"})
      */
-    public function details($id)
+    public function details($id, IdeaRepository $ideaRepository)
     {
-        //@todo: aller chercher cette idée dans la bdd
-        dump($id);
+        //ou aussi
+        //$entityManager = $this->getDoctrine()->getManager();
+        //$ideaRepository = $entityManager->getRepository(Idea::class);
+
+        //récupère une Idea en fonction de la clé primaire
+        $idea = $ideaRepository->find($id);
 
         return $this->render('idea/details.html.twig', [
-
+            "idea" => $idea
         ]);
     }
 }

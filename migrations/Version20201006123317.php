448d84fa4ae11e860e712722de1fbeae578d6a85
Guillaume <gsylvestre@gmail.com>
2020-10-06 15:03:52 +0200

    crée la migration originelle

---
+++ migrations/Version20201006123317.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201006123317 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(40) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE idea (id INT AUTO_INCREMENT NOT NULL, category_id INT NOT NULL, title VARCHAR(250) NOT NULL, description LONGTEXT DEFAULT NULL, author VARCHAR(30) NOT NULL, is_published TINYINT(1) NOT NULL, date_created DATETIME NOT NULL, INDEX IDX_A8BCA4512469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(180) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE idea ADD CONSTRAINT FK_A8BCA4512469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE idea DROP FOREIGN KEY FK_A8BCA4512469DE2');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE idea');
+        $this->addSql('DROP TABLE user');
+    }
+}
